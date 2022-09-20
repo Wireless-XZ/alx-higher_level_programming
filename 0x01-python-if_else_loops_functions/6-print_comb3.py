e2843fb9cdcb0125f838a540ca9e2e753a8663c8
xz <odufeso1@gmail.com>
2022-09-20 16:06:52 -0700

    Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+x = 0
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (((i + 1) == 10 or (i + 2) == 10)
+            and j == 9):
+            print("{}{}".format(i, j))
+            x = 1
+            break;
+        elif not (j <= i):
+            print("{}{},".format(i, j), end=" ")
+    if x == 1:
+        break

1feb8d767d24f47860af2ee21e28962d6d8ffa1d
xz <odufeso1@gmail.com>
2022-09-20 16:10:59 -0700

    pycodestyle fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,11 +2,10 @@
 x = 0
 for i in range(0, 10):
     for j in range(0, 10):
-        if (((i + 1) == 10 or (i + 2) == 10)
-            and j == 9):
+        if (((i + 1) == 10 or (i + 2) == 10) and j == 9):
             print("{}{}".format(i, j))
             x = 1
-            break;
+            break
         elif not (j <= i):
             print("{}{},".format(i, j), end=" ")
     if x == 1:

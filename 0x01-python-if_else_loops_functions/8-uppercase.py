b4cbff64e6918f475ff3eee26c122ecb82d543f4
xz <odufeso1@gmail.com>
2022-09-20 17:06:23 -0700

    Task *

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    up_list = []
+    for i in str:
+        if ord(i) in range(ord("a"), ord("{")):
+           up_list.append(chr(ord(i) - 32))
+        else:
+            up_list.append(i)
+
+    for j in up_list:
+        print("{}".format(j), end="")
+    print("")

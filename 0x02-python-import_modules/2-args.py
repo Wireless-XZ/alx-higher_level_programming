2966ff4f412ae9d81d3e310eebc9043844dd6821
xz <odufeso1@gmail.com>
2022-09-22 10:34:23 -0700

    Bug fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 def print_argv():
     x = 0
     for i in argv:
@@ -8,11 +9,13 @@ def print_argv():
             print("{}: {}".format(x, i))
         x += 1
 
-if len(argv) - 1 == 1:
-    print("{} argument:".format(len(argv) - 1))
-    print_argv()
-elif len(argv) - 1 == 0:
-    print("{} arguments.".format(len(argv) - 1))
-else:
-    print("{} arguments:".format(len(argv) -1 ))
-    print_argv()
+
+if __name__ == "__main__":
+    if len(argv) - 1 == 1:
+        print("{} argument:".format(len(argv) - 1))
+        print_argv()
+    elif len(argv) - 1 == 0:
+        print("{} arguments.".format(len(argv) - 1))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        print_argv()

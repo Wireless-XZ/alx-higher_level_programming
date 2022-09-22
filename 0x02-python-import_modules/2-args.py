956c6312d9b4eb2ba36820760d12bd0ffca4c076
xz <odufeso1@gmail.com>
2022-09-22 10:29:31 -0700

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from sys import argv
+
+def print_argv():
+    x = 0
+    for i in argv:
+        if x != 0:
+            print("{}: {}".format(x, i))
+        x += 1
+
+if len(argv) - 1 == 1:
+    print("{} argument:".format(len(argv) - 1))
+    print_argv()
+elif len(argv) - 1 == 0:
+    print("{} arguments.".format(len(argv) - 1))
+else:
+    print("{} arguments:".format(len(argv) -1 ))
+    print_argv()

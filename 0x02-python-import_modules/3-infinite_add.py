36c5e36745042a45c87951039b6506ca6be12fca
xz <odufeso1@gmail.com>
2022-09-22 10:56:03 -0700

    Bug fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
 
-if len(argv) - 1 == 0: print("0")
-else:
-    i, sum_ = 1, 0
-    while i < len(argv):
-        sum_ += int(argv[i])
-        i += 1
-    print(sum_)
+if __name__ == "__main__":
+    if len(argv) - 1 == 0:
+        print("0")
+    else:
+        i, sum_ = 1, 0
+        while i < len(argv):
+            sum_ += int(argv[i])
+            i += 1
+        print(sum_)

f4655cbb3aea464ec35797874a17c77fe645ca14
xz <odufeso1@gmail.com>
2022-09-24 06:55:08 -0700

    Task 7

--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -3,9 +3,12 @@ add_tuple = __import__('7-add_tuple').add_tuple
 
 tuple_a = (1, 89)
 tuple_b = (88, 11)
+tuple_c = (1, 89, 139)
+tuple_d = (88, 11, 908)
 new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
 
 print(add_tuple(tuple_a, (1, )))
 print(add_tuple(tuple_a, ()))
 print(add_tuple((), ()))
+print(add_tuple(tuple_c, tuple_d))

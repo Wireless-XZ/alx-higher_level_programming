f7a57ba9c938482c1f7744295f011d5795a2e757
xz <odufeso1@gmail.com>
2022-09-25 04:55:38 -0700

    Bug fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <string.h>
+#include <stdio.h>
 
 /**
  * is_palindrome - Write a function in C that checks
@@ -9,23 +11,35 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int len, i = 0, n;
-	char *array;
+	int len, i, x;
+	char *array = NULL;
+	listint_t *dummy;
 
-	if (head == NULL)
+	dummy = *head;
+	if (*head == NULL)
 		return (1);
-	for (len = 0; *head; len++)
-		*head = (*head)->next;
-	n = len - 1;
-	array = malloc(sizeof(int) * (len + 1));
+	printf("LOL 1\n");
+	for (len = 0; dummy; len++)
+		dummy = dummy->next;
+	printf("list_len: %d\n", len);
+	dummy = *head;
+	x = len - 1;
+	printf("Dummy: %d\n", dummy->n);
+	array = malloc((sizeof(int) * len) + 1);
 	if (array == NULL)
 		return (-1);
-	for (i = 0; *head; i++)
-		array[i] = (*head)->n;
-	array[i] = '\0';
-	for (i = 0; n > i; i++, n--)
+	printf("Malloc pass\n");
+	for (i = 0; dummy; i++)
 	{
-		if (array[i] != array[n])
+		printf("%d ", i);
+		array[i] = dummy->n;
+		dummy = dummy->next;
+	}
+	array[i--] = '\0';
+	printf("len: %d\n", i);
+	for (x = 0; i > x; x++, i--)
+	{
+		if (array[x] != array[i])
 		{
 			free(array);
 			return (0);

42ca30fe1b8be6a1a4f81ccbb0ae40a34a905a6c
xz <odufeso1@gmail.com>
2022-09-28 14:20:45 -0700

    Bug fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = my_list.copy()
-    if search in my_list:
-        for j in new:
-            if j == search:
-                new[j - 1] = replace
+    for j in new:
+        if j == search:
+            new[j - 1] = replace
     return new

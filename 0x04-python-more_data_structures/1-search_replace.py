fed725b56d1539904f0c0d49447ceac8d2125c01
xz <odufeso1@gmail.com>
2022-09-28 09:02:03 -0700

    Bug fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = my_list.copy()
-    for j in new:
-        if j == search:
-            new[j - 1] = replace
+    if search in my_list:
+        for j in new:
+            if j == search:
+                new[j - 1] = replace
     return new

5fac9c2b6ab4a057a6fb5c6754c438b228aeab39
xz <odufeso1@gmail.com>
2022-09-28 12:50:31 -0700

    Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    for i in sorted(a_dictionary.values(), reverse=True):
+        for j, k in a_dictionary.items():
+            if i == k:
+                return j

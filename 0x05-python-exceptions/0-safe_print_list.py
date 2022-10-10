5cf4797e7baf5449b92c4fdc74943a239e32528b
xz <odufeso1@gmail.com>
2022-10-10 16:40:21 -0700

    Bug fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     len = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
             len += 1
         print()
         return len

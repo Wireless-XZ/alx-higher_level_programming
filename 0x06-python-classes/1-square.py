0f7b5289280c0a6469eefd9d9b3bc83089d983fc
xz <odufeso1@gmail.com>
2022-10-11 11:13:35 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Creates a Square object"""
+    def __init__(self, size):
+        """Instantiate Square object with size attribute"""
+        self.__size = size

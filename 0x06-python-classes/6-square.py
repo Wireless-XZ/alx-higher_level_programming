99be459f8cfb00a6dc61d38db09273839e3e36f9
xz <odufeso1@gmail.com>
2022-10-11 13:18:14 -0700

    Bug fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -82,17 +82,15 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def my_print(self):
-        """Prints out a grid with the char #
+        """prints the square
 
         Returns: None
         """
-        if (self.__position)[1] == 1:
-            print()
-        for i in range(self.__size):
-            for k in range((self.__position[0])):
-                print(end=" ")
-            for j in range(self.__size):
-                print("#", end="")
-            print()
         if self.__size == 0:
             print()
+            return
+        for i in range(self.__position[1]):
+            print()
+        for j in range(self.__size):
+            print("".join([" " for k in range(self.__position[0])]), end="")
+            print("".join(["#" for z in range(self.__size)]))

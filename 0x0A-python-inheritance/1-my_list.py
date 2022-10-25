73ca9a694ef51cf92034214fff1f20586c71c138
xz <odufeso1@gmail.com>
2022-10-24 18:35:48 -0700

    Bug fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,5 +4,9 @@
 
 
 class MyList(list):
+    """ A class MyList that inherits from list
+    """
     def print_sorted(self):
+        """ prints the list, but sorted
+        """
         print(sorted(self))

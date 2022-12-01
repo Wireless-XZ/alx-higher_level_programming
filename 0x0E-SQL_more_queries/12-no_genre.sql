2e136642cc8dc12b800132077d3b5500e77c2d02
xz <odufeso1@gmail.com>
2022-11-30 18:47:55 -0800

    Bug fix

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT OUTER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id is NULL;
+WHERE tv_show_genres.show_id is NULL;

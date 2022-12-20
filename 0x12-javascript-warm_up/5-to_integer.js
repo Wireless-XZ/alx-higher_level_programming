1674e7ed5bc317fc2a1a180ce1d66e47f9d31e04
xz <odufeso1@gmail.com>
2022-12-19 16:56:35 -0800

    Bug fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const args = process.argv;
 if (args[2] && Number.parseInt(args[2])) {
   console.log('My number: ' + Number.parseInt(args[2]));
 } else {
-  console.log('Not a number')
+  console.log('Not a number');
 }

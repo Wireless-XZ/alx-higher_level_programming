998140655ce89101b91240eb6f52a584642e068b
xz <odufeso1@gmail.com>
2022-12-27 05:45:56 -0800

    Bug Fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,13 +2,13 @@
 """
  lists all State objects from the database hbtn_0e_6_usa
 """
-from model_state import State
+from model_state import State, Base
 from sqlalchemy import create_engine
 import sys
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                        pool_pre_ping=True)
 
